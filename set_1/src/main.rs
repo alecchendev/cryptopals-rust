35a18cd3fcb9c660344f47e98c0baaeba030d8c5
Alec Chen <alecchendev@gmail.com>
2023-01-19 14:31:20 -0600

    Complete set 1 challenge 6

--- set_1/src/main.rs
+++ set_1/src/main.rs
@@ -3,10 +3,162 @@ use std::collections::HashMap;
 use base64::{engine::general_purpose, Engine};
 use hex;
 use std::fs;
-use std::io::Read;
+use std::io::prelude::*;
 
 fn main() {
     println!("Hello, world!");
+}
+
+// Challenge 6
+
+fn get_best_key_size(input: &Vec<u8>) -> usize {
+    get_key_sizes(input)[0]
+}
+
+fn get_key_sizes(input: &Vec<u8>) -> Vec<usize> {
+    let candidate_key_sizes = 2..40;
+    let mut key_size_scores = Vec::new();
+    for key_size in candidate_key_sizes {
+        let score = get_key_size_score(input, key_size);
+        key_size_scores.push((score, key_size));
+    }
+    key_size_scores.sort();
+    key_size_scores.iter().map(|&(_score, key)| key).collect()
+}
+
+fn get_key_size_score(input: &Vec<u8>, key_size: usize) -> u32 {
+    let n_blocks = 4;
+    assert!(input.len() > key_size * n_blocks);
+
+    let mut blocks = Vec::new();
+    for i in 0..n_blocks {
+        blocks.push(&input[(i * key_size)..((i + 1) * key_size)]);
+    }
+
+    let mut score = 0;
+    for i in 0..(n_blocks - 1) {
+        let block_i = blocks[i];
+        for j in (i + 1)..n_blocks {
+            let block_j = blocks[j];
+            let distance = hamming_distance(block_i.to_vec(), block_j.to_vec());
+            score += normalize_distance(distance, key_size);
+        }
+    }
+
+    score
+}
+
+fn decrypt_repeating_key_xor(input: Vec<u8>) -> Vec<u8> {
+    // Get key_size
+    let key_size = get_best_key_size(&input);
+
+    // Break into key_size blocks
+    let blocks = get_blocks(&input, key_size);
+
+    // Transpose blocks
+    let transposed = transpose_blocks(&blocks);
+
+    // Single byte xor decrypt blocks
+    let mut key = vec![];
+    for block in transposed.into_iter() {
+        let (_score, single_byte_key, _plaintext) = decrypt_single_byte_xor(block);
+        key.push(single_byte_key);
+    }
+
+    // Decrypt
+    repeating_key_xor(key, input.clone())
+}
+
+#[test]
+fn test_decrypt_repeating_key_xor() {
+    let mut file = fs::File::open("data/6.txt").unwrap();
+    let mut base64_contents = String::new();
+    file.read_to_string(&mut base64_contents).unwrap();
+    base64_contents = base64_contents.replace("\n", "");
+    let contents = general_purpose::STANDARD.decode(base64_contents).unwrap();
+
+    let mut file = fs::File::open("data/6_sol.txt").unwrap();
+    let mut expected_output = String::new();
+    file.read_to_string(&mut expected_output).unwrap();
+
+    let output = decrypt_repeating_key_xor(contents);
+    assert_eq!(output, expected_output.into_bytes());
+}
+
+fn transpose_blocks(blocks: &Vec<Vec<u8>>) -> Vec<Vec<u8>> {
+    assert!(blocks.len() > 0);
+    let key_size = blocks[0].len();
+    let mut transposed = vec![];
+    for col in 0..key_size {
+        let mut block = vec![];
+        for row in 0..blocks.len() {
+            if blocks[row].len() <= col {
+                break;
+            }
+            block.push(blocks[row][col]);
+        }
+        transposed.push(block);
+    }
+    transposed
+}
+
+#[test]
+fn test_transpose_blocks() {
+    let blocks = vec![vec![1, 2, 3], vec![4, 5]];
+    assert_eq!(
+        transpose_blocks(&blocks),
+        vec![vec![1, 4], vec![2, 5], vec![3]]
+    );
+}
+
+fn div_ceil(a: usize, b: usize) -> usize {
+    (a + b - 1) / b
+}
+
+fn get_blocks(input: &Vec<u8>, key_size: usize) -> Vec<Vec<u8>> {
+    let mut blocks = vec![];
+    let n_blocks = div_ceil(input.len(), key_size);
+    for block_idx in 0..n_blocks {
+        let start = block_idx * key_size;
+        let end = std::cmp::min(start + key_size, input.len());
+        blocks.push(input[start..end].to_vec());
+    }
+    blocks
+}
+
+#[test]
+fn test_get_blocks() {
+    let input = vec![1, 2, 3, 4, 5];
+    assert_eq!(get_blocks(&input, 3), vec![vec![1, 2, 3], vec![4, 5]]);
+    assert_eq!(get_blocks(&input, 5), vec![vec![1, 2, 3, 4, 5]]);
+}
+
+fn normalize_distance(distance: u32, key_size: usize) -> u32 {
+    distance * 10000 / (8 * key_size as u32)
+}
+
+fn hamming_distance(input1: Vec<u8>, input2: Vec<u8>) -> u32 {
+    assert!(input1.len() == input2.len());
+    let mut distance = 0;
+    for i in 0..input1.len() {
+        let byte1 = input1[i];
+        let byte2 = input2[i];
+        let diff_bits = byte1 ^ byte2;
+        for bit in 0..8 {
+            distance += (diff_bits & (1 << bit) != 0) as u32;
+        }
+    }
+    distance
+}
+
+#[test]
+fn test_hamming_distance() {
+    let input1 = String::from("this is a test").into_bytes();
+    let input2 = String::from("wokka wokka!!!").into_bytes();
+    let expected_output = 37;
+
+    let output = hamming_distance(input1, input2);
+    assert_eq!(output, expected_output);
 }
 
 // Challenge 5
