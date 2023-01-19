1f5b52289183483718497343754c4717d4e8b5fd
Alec Chen <alecchendev@gmail.com>
2023-01-19 10:47:46 -0600

    Complete set 1 challenge 5

--- set_1/src/main.rs
+++ set_1/src/main.rs
@@ -9,6 +9,37 @@ fn main() {
     println!("Hello, world!");
 }
 
+// Challenge 5
+
+fn repeating_key_xor(key: Vec<u8>, input: Vec<u8>) -> Vec<u8> {
+    assert!(key.len() > 0);
+    assert!(input.len() > 0);
+    let mut output = Vec::new();
+    let mut key_idx = 0;
+    for byte in input.iter() {
+        output.push(byte ^ key[key_idx]);
+        key_idx = (key_idx + 1) % key.len();
+    }
+    output
+}
+
+#[test]
+fn test_repeating_key_xor() {
+    let key = String::from("ICE").into_bytes();
+    let input = String::from(
+        "Burning 'em, if you ain't quick and nimble
+I go crazy when I hear a cymbal",
+    )
+    .into_bytes();
+    let expected_output = hex::decode(String::from(
+        "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f",
+    ))
+    .unwrap();
+
+    let output = repeating_key_xor(key, input);
+    assert_eq!(output, expected_output);
+}
+
 // Challenge 4
 
 fn decrypt_single_byte_xor_many(lines: Vec<String>) -> (u32, u8, Vec<u8>) {
