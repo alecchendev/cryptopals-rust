ae8dd687e6cb2b71b0984c50b95352a1c14de19a
Alec Chen <alecchendev@gmail.com>
2023-01-18 21:40:34 -0600

    Complete challenge 2

--- set_1/src/main.rs
+++ set_1/src/main.rs
@@ -5,6 +5,36 @@ fn main() {
     println!("Hello, world!");
 }
 
+// Challenge 2
+
+fn fixed_xor(input1: &[u8], input2: &[u8]) -> Vec<u8> {
+    assert_eq!(input1.len(), input2.len());
+    let n = input1.len();
+    let mut result = Vec::new();
+    for i in 0..n {
+        result.push(input1[i] ^ input2[i]);
+    }
+    result
+}
+
+#[test]
+fn test_fixed_xor() {
+    let input1 = String::from("1c0111001f010100061a024b53535009181c");
+    let input2 = String::from("686974207468652062756c6c277320657965");
+    let expected_output = String::from("746865206b696420646f6e277420706c6179");
+
+    let output = fixed_xor(
+        hex::decode(input1).unwrap().as_slice(),
+        hex::decode(input2).unwrap().as_slice(),
+    );
+    assert_eq!(
+        hex::decode(expected_output).unwrap().as_slice(),
+        output.as_slice()
+    );
+}
+
+// Challenge 1
+
 fn hex_to_base64(input: &[u8]) -> Vec<u8> {
     let normal = hex::decode(input).unwrap();
     general_purpose::STANDARD_NO_PAD.encode(normal).into_bytes()
@@ -19,8 +49,8 @@ fn test_hex_to_base64() {
     assert_eq!(expected_output, output.as_slice());
 }
 
-fn hex_to_base64_string(hex: String) -> String {
-    let normal = hex::decode(hex).unwrap();
+fn hex_to_base64_string(input: String) -> String {
+    let normal = hex::decode(input).unwrap();
     general_purpose::STANDARD_NO_PAD.encode(normal)
 }
 
