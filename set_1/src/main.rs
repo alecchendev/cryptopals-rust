c54edd36619871587e4b3ea87e84cde64d1e2223
Alec Chen <alecchendev@gmail.com>
2023-01-18 21:14:04 -0600

    Complete challenge 1

--- set_1/src/main.rs
+++ set_1/src/main.rs
@@ -1,3 +1,35 @@
+use base64::{engine::general_purpose, Engine};
+use hex;
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn hex_to_base64(input: &[u8]) -> Vec<u8> {
+    let normal = hex::decode(input).unwrap();
+    general_purpose::STANDARD_NO_PAD.encode(normal).into_bytes()
+}
+
+#[test]
+fn test_hex_to_base64() {
+    let input = b"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    let expected_output = b"SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+
+    let output = hex_to_base64(input);
+    assert_eq!(expected_output, output.as_slice());
+}
+
+fn hex_to_base64_string(hex: String) -> String {
+    let normal = hex::decode(hex).unwrap();
+    general_purpose::STANDARD_NO_PAD.encode(normal)
+}
+
+#[test]
+fn test_hex_to_base64_string() {
+    let input = String::from("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
+    let expected_output =
+        String::from("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t");
+
+    let output = hex_to_base64_string(input);
+    assert_eq!(expected_output, output);
 }
